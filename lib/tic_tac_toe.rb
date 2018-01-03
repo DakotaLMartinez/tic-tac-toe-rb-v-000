a5b183e7162459dab473becfa907ac20148a7078
Dakota Martinez <dakotaleemusic@gmail.com>
2018-01-03 17:48:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,7 +40,7 @@ def turn(board)
   index = input_to_index(input)
   
   if valid_move?(board, index)
-    token = current_player
+    token = current_player(board)
     binding.pry
     move(board, index, token)
   end
